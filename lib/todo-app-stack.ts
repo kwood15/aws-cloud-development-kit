f2957b84f4cdce0f58b5c1fd9013ff4b22490673
Kay Wood <kwood15@users.noreply.github.com>
2020-10-09 10:09:21 +0100

    Update dir path for lambda function

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -1,14 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as path from 'path';
 
 export class TodoAppStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const helloLambda = new lambda.Function(this, 'HelloLambda', {
-      code: lambda.Code.fromAsset('lambda'),
+      runtime: lambda.Runtime.NODEJS_12_X,
       handler: 'hello.handler',
-      runtime: lambda.Runtime.NODEJS_12_X
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda'))  
     });
   }
 }
