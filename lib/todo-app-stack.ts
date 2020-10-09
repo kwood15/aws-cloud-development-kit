6c47e6fa317d6f307a611f803725438e21d4c466
Kay Wood <kwood15@users.noreply.github.com>
2020-10-09 11:12:28 +0100

    Add env var to lambda

--- lib/todo-app-stack.ts
+++ lib/todo-app-stack.ts
@@ -12,7 +12,10 @@ export class TodoAppStack extends cdk.Stack {
       handler: 'hello.handler',
       code: lambda.Code.fromAsset(path.join(__dirname, 'lambda')),
       memorySize: 256,
-      timeout: cdk.Duration.seconds(10)
+      timeout: cdk.Duration.seconds(10),
+      environment: {
+        isProduction: 'not production'
+      }
     });
 
     new apiGateway.LambdaRestApi(this, 'Endpoint', {
